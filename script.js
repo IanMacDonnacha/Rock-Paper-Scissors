c11e8cf4945721c3d15304aca4c1973aab95148c
IanMacDonnacha <Ian.macdonnacha@gmail.com>
2023-02-14 20:27:00 +0000

    Atempt to have funtion generate random 'rock' 'paper' or 'scissors'

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-function getComputerChoice(max) {
-    return Math.floor(Math.random() * max);
+function getComputerChoice(choice) {
+    choice = ['Rock', 'Paper', 'Scissors']
+    return Math.floor(Math.random() * choice.length );
 }
 console.log(getComputerChoice(3))
