4b419ca10f71e9b553aa72c602f291c010c233c0
IanMacDonnacha <Ian.macdonnacha@gmail.com>
2023-04-02 20:25:49 +0100

    Add playGame function to compare playerchoice and computerchoice

--- script.js
+++ script.js
@@ -1,12 +1,37 @@
 function generateChoice() {
     choice = Math.floor(Math.random() * 3 );
     if (choice === 0) {
-        return "Rock"
-    } else if (choice === 1) {
-        return "Paper"
+        return "Rock";
+    }else if (choice === 1) {
+        return "Paper";
     }else {
-        return "Scissors"
+        return "Scissors";
+    }
+};
+console.log(generateChoice())
+
+//Lots of repetitive code here.
+function playGame(playerSelection, computerSelection){
+    player = playerSelection.toLowerCase()
+    computer = computerSelection.toLowerCase()
+    if (player === "rock" && computer === "scissors"){
+        return "Player, you are the winner!"
+    }else if (player === "rock" && computer === "paper"){
+        return "Computer, you are the winner!"
+    }else if (player === "paper" && computer === "scissors"){
+        return "Computer, you are the winner!"
+    }else if (player === "paper" && computer === "rock"){
+        return "Player, you are the winner!"
+    }else if (player === "scissors" && computer === "rock"){
+        return "Computer, you are the winner!"
+    }else if (player === "scissors" && computer === "paper"){
+        return "Player, you are the winner!"
+    }else {
+        return "It is a draw!"
     }
 }
 
-console.log(generateChoice())
+const playerSelection = "Rock"
+const computerSelection = generateChoice()
+
+console.log(playGame(playerSelection,computerSelection))
