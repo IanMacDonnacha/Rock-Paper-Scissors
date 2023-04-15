6bf94786bf302d00c31b6956409f985fa0021558
IanMacDonnacha <Ian.macdonnacha@gmail.com>
2023-04-15 18:18:12 +0100

    Update eventlisteners to start game with button

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
+const rock = document.querySelector(".rock");
+const paper = document.querySelector(".paper");
+const scissors = document.querySelector(".scissors");
+
+    
+rock.addEventListener("click", () => {
+    game("Rock");
+})
+paper.addEventListener("click", () =>{
+    game("Paper");
+})
+scissors.addEventListener("click", () => {
+    game("Scissors");
+})
+
+
+
 function generateChoice() {
     choice = Math.floor(Math.random() * 3 );
     if (choice === 0) {
@@ -10,39 +27,42 @@ function generateChoice() {
 };
 
 function playGame(playerSelection, computerSelection){
-    player = playerSelection.toLowerCase();
-    computer = computerSelection.toLowerCase();
+    player = playerSelection;
+    computer = computerSelection;
 
-    if (player === "rock" && computer === "scissors"){
+    if (player === "Rock" && computer === "Scissors"){
         return 1;
-    }else if (player === "rock" && computer === "paper"){
+    }else if (player === "Rock" && computer === "Paper"){
         return 0;
-    }else if (player === "paper" && computer === "scissors"){
+    }else if (player === "Paper" && computer === "Scissors"){
         return 0;
-    }else if (player === "paper" && computer === "rock"){
+    }else if (player === "Paper" && computer === "Rock"){
         return 1;
-    }else if (player === "scissors" && computer === "rock"){
+    }else if (player === "Scissors" && computer === "Rock"){
         return 0;
-    }else if (player === "scissors" && computer === "paper"){
+    }else if (player === "Scissors" && computer === "Paper"){
         return 1;
     }else {
         return "It is a draw!"
-    }
+    }    
 }
 
-function game(){
+
+function game(playerSelection){
+    let rounds = 0
     let playerScore = 0;
     let computerScore = 0;
-    for (let i = 0; i < 5; i++){
-        let playerSelection = prompt("Rock, Paper or Scissors?: ")
-        let computerSelection = generateChoice();
-        let game = playGame(playerSelection, computerSelection);
-        console.log(game)
+    while (rounds < 5){
+        let cpuChoice = generateChoice();
+        let game = playGame(playerSelection, cpuChoice);
+        console.log(playerSelection)
+        console.log(cpuChoice)
         if (game == 1){
-            playerScore += 1;
+            playerScore ++;
         } else {
-            computerScore += 1;
+            computerScore ++;
         }
+        rounds++;
     }
     if (playerScore > computerScore){
         return `Player you won ${playerScore} rounds out of 5, you are the winner!`
